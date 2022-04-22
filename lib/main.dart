291cfc3f8a0fce3e5ca5c5647e187a7570414130
Adolfo <adolfovladimir.r.v@gmail.com>
2022-04-21 22:18:06 -0500

    cambio de nombre

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MandarinaApp extends StatelessWidget {
               height: 10.0,
             ),
             Text(
-              "Fiorella Franco",
+              "Fiorella Fran",
               style: TextStyle(
                   color: Colors.white,
                   fontSize: 22.0,
